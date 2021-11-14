2d25e15ddfddaddbe7766ad428fe157124972fcc
Jean-Remi Avargues <jean-remi>
2021-11-14 14:37:39 +0100

    Generate sticky notes when dragged from an icon and rectangle resizable but both features are not combined

--- koala-note/src/App.js
+++ koala-note/src/App.js
@@ -1,36 +1,166 @@
-import './App.css';
+import React, { useState, useRef } from "react";
+import { Stage, Layer, Rect, Text, Transformer } from 'react-konva';
 
-import { Button } from '@material-ui/core';
-import { Stage, Layer, Rect, Circle } from 'react-konva';
+const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
 
-function App() {
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
 
   return (
-    <div className="background">
-      <Button
-        variant="contained"
-        color="blue"
-        style={{ width: "10%", height: "15%" }}
-        onClick= {() => {
+    <React.Fragment>
+      <Rect
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
         }}
-      >
-      Create circle
-      </Button>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Rect width={50} height={50} fill="red" />
-          <Circle
-            x={window.innerWidth / 2 - 50}
-            y={window.innerHeight / 2 - 25}
-            stroke="black"
-            radius={50}
-            fill="blue"
-            draggable={true}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+const initialRectangles = [
+  {
+    x: 300,
+    y: 10,
+    width: 100,
+    height: 100,
+    fill: 'red',
+    id: 'rect1',
+  },
+  {
+    x: 400,
+    y: 150,
+    width: 100,
+    height: 100,
+    fill: 'green',
+    id: 'rect2',
+  },
+];
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+  const stageRef = useRef(null);
+  const [rectangles, setRectangles] = React.useState(initialRectangles);
+  const [selectedId, selectShape] = React.useState(null);
+
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
+
+  var randomColor = require('randomcolor');
+
+  return (
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={checkDeselect}
+      onTouchStart={checkDeselect}
+      ref={stageRef}
+    >
+      <Layer>
+        <Text
+          fontSize={20}
+          text="Drag and drop sticky notes"
+          align="center"
+        />
+        <Rect
+          name="stickyNote"
+          x={50}
+          y={50}
+          width={25}
+          height={25}
+          fill="yellow"
+          draggable
+          onDragEnd={(e) => {
+            setNotes((prevNotes) => [
+              ...prevNotes,
+              { x: e.target.x(), y: e.target.y(), fill: randomColor() }
+            ]);
+            var stage = stageRef.current;
+            var stickyNote = stage.findOne(".stickyNote");
+            stickyNote.position({ x: 50, y: 50});
+          }}
+        />
+        {notes.map((eachNote) => (
+          <Rect
+            x={eachNote.x}
+            y={eachNote.y}
+            width={200}
+            height={200}
+            fill={eachNote.fill}
+            draggable
           />
-        </Layer>
-      </Stage>
-    </div>
+        ))}
+        {rectangles.map((rect, i) => {
+          return (
+            <Rectangle
+              key={i}
+              shapeProps={rect}
+              isSelected={rect.id === selectedId}
+              onSelect={() => {
+                selectShape(rect.id);
+              }}
+              onChange={(newAttrs) => {
+                const rects = rectangles.slice();
+                rects[i] = newAttrs;
+                setRectangles(rects);
+              }}
+            />
+          );
+        })}
+      </Layer>
+    </Stage>
   );
 }
-
-export default App;
